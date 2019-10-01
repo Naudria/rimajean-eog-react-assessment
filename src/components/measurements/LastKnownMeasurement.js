cad3353d1cc83eb71d10f1310eed629aba6797cc
Naudria <rima.jean@gmail.com>
2019-10-01 08:48:57 -0500

    Add material ui styles and graphql query for LastKnownMeasurment

--- src/components/measurements/LastKnownMeasurement.js
+++ src/components/measurements/LastKnownMeasurement.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { Provider, createClient, useQuery } from 'urql';
 import { useDispatch } from 'react-redux';
 import * as actions from '../../store/actions';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2)
+  }
+}));
 
 const client = createClient({
   url: 'https://react.eogresources.com/graphql'
@@ -11,6 +21,19 @@ const LastKnownMeasurement = props => {
   const { metric } = props;
 
   const [measurement, setMeasurement] = useState({metric});
+
+  const classes = useStyles();
+
+  const query = `
+		query {
+		  getLastKnownMeasurement(metricName: "${metric.metricName}") {
+		    metric
+		    value
+		    unit
+		    at
+		  }
+		}
+		`;
 
 }
 
