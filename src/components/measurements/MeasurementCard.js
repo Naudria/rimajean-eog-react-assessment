dbbf32aaff249791ce1c867cab3f72ec76ba081d
Naudria <rima.jean@gmail.com>
2019-10-01 09:06:13 -0500

    MetricSelect object useEffect hook to update metric data

--- src/components/measurements/MeasurementCard.js
+++ src/components/measurements/MeasurementCard.js
@@ -6,6 +6,28 @@ import LastKnownMeasurement from './LastKnownMeasurement'
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    marginLeft: 50,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  formControl: {
+    width: 120
+  },
+  select: {
+    width: 400,
+  },
+  card: {
+    textAlign: 'center',
+    margin: '5%'
+  }
+}));
+
 const client = createClient({
   url: 'https://react.eogresources.com/graphql'
 });
@@ -36,3 +58,69 @@ const MeasurementCard = () => {
     </Card>
   );
 };
+
+const MetricSelect = () => {
+  const classes = useStyles();
+
+  const [metrics, setMetrics] = useState();
+
+  const [selectedMetric, setSelectedMetric] = useState('');
+
+  const dispatch = useDispatch();
+
+  const [result] = useQuery({
+    query
+  });
+
+  const { fetching, data, error } = result;
+
+  const handleChange = event => {
+    setSelectedMetric(event.target.value);
+  };
+
+  useEffect(() => {
+    if (error) {
+      dispatch({
+        type: actions.API_ERROR,
+        error: error.message
+      });
+      return;
+    }
+    if (!data) return;
+    const metricData = Object.values(data.getMetrics);
+    setMetrics(metricData);
+  }, [dispatch, data, error]);
+
+  if (fetching) return <LinearProgress />;
+
+  return (
+
+    <form className={classes.root}>
+        
+      <FormControl classess={classes.formControl}>
+        <InputLabel htmlor="metric-select">Choose Metric</InputLabel>
+        <Select
+          className={classes.select}
+          value={selectedMetric}
+          inputProps={{
+            name: 'metric',
+            id: 'metric-select'
+          }}
+          onChange={handleChange}
+        >
+          {metrics
+            ? metrics.map((metric, index) => (
+                <MenuItem value={metric} key={index}>
+                  {metric}
+                </MenuItem>
+              ))
+            : null}
+        </Select>
+        <LastKnownMeasurement metricName={selectedMetric} />
+      </FormControl>
+      <MeasurementChart metricName={selectedMetric} />
+          
+    </form>
+  );
+};
+
